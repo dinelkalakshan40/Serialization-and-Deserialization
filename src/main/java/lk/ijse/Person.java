95529a1f4f45e7d95bcc5b5323e8c4f09f713ecf
dinelka <newlakshan3@gmail.com>
2024-07-19 00:36:03 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/Person.java
@@ -0,0 +1,9 @@
+package lk.ijse;
+
+import java.io.Serializable;
+
+public class Car implements Serializable {
+    private static final long serialVersionUID=1l;
+    private String code;
+    private
+}

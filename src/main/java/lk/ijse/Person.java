f196ccc35c8d7c70edb7c7990740da1e7cb89227
dinelka <newlakshan3@gmail.com>
2024-07-19 12:26:48 +0530

    update

--- src/main/java/lk/ijse/Person.java
+++ src/main/java/lk/ijse/Person.java
@@ -7,8 +7,8 @@ public class Person implements Serializable {
     private String name;
     private int id;
 
-    public Person(String code, int id) {
-        this.name = code;
+    public Person(String name, int id) {
+        this.name = name;
         this.id = id;
     }
 
